13413e3040873f8d8706884f0a46c48c1ee874f3
Volodymyr <vtsiumra@yahoo.com>
2021-11-06 14:10:32 +0100

    Add solution to task 1

---
+++ src/Level1/Factorial.java
@@ -0,0 +1,19 @@
+package Level1;
+
+import java.math.BigInteger;
+
+public class Factorial implements Runnable {
+    public BigInteger factorial(int n){
+        BigInteger result = BigInteger.ONE;
+        for(int i = 1; i<=n; i++){
+            result = BigInteger.valueOf(i).multiply(result);
+        }
+        return result;
+    }
+
+    @Override
+    public void run() {
+        int id = Integer.parseInt(Thread.currentThread().getName());
+        System.out.println(Thread.currentThread().getName() + " - id: " + Thread.currentThread().getId() +" factorial: " + this.factorial(id));
+    }
+}
--- src/Level1/Main.java
+++ src/Level1/Main.java
@@ -1,4 +1,12 @@
 package Level1;
 
 public class Main {
+    public static void main(String[] args) {
+
+        for(int i = 0; i < 100; i++){
+            Thread thread = new Thread(new Factorial());
+            thread.setName(String.valueOf(i));
+            thread.start();
+        }
+    }
 }
